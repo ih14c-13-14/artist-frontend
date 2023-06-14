7e9e6a70887d7450820c215c3f28643c631135d3
aogumobc <aogumobc@gmail.com>
2023-06-14 12:04:11 +0900

    NormalButton

--- src/components/form/inputs/NormalButton/NormalButton.component.tsx
+++ src/components/form/inputs/NormalButton/NormalButton.component.tsx
@@ -3,13 +3,20 @@ import { ButtonProps } from './NormalButton.type';
 
 export const NormalButton = ({
   label,
+  onClick,
   variant = 'contained',
   disabled,
   color = 'error',
   ...rest
 }: ButtonProps) => {
   return (
-    <MuiButton variant={variant} disabled={disabled} color={color} {...rest}>
+    <MuiButton
+      variant={variant}
+      disabled={disabled}
+      color={color}
+      onClick={onClick}
+      {...rest}
+    >
       {label}
     </MuiButton>
   );
--- src/components/form/inputs/NormalButton/NormalButton.type.ts
+++ src/components/form/inputs/NormalButton/NormalButton.type.ts
@@ -2,4 +2,5 @@ import { ButtonProps as MuiButtonProps } from '@mui/material/Button';
 
 export type ButtonProps = MuiButtonProps & {
   label: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement> | undefined;
 };
