705992ae970963b53fba6ee77253bb56659b8097
aogumobc <aogumobc@gmail.com>
2023-06-14 12:45:09 +0900

    add: disabled

--- src/components/form/inputs/NormalButton/NormalButton.stories.tsx
+++ src/components/form/inputs/NormalButton/NormalButton.stories.tsx
@@ -14,16 +14,19 @@ const Template: StoryFn<typeof NormalButton> = args => (
 export const Contained = Template.bind({});
 Contained.args = {
   label: 'Normal',
+  disabled: false,
 };
 
 export const Outlined = Template.bind({});
 Outlined.args = {
   label: 'Normal',
   variant: 'outlined',
+  disabled: false,
 };
 
 export const Text = Template.bind({});
 Text.args = {
   label: 'Normal',
   variant: 'text',
+  disabled: false,
 };
