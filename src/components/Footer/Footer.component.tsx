a1b9b83edb7f349c71386401dcbd99a912de57ee
aogumobc <aogumobc@gmail.com>
2023-06-22 12:23:56 +0900

    yarn lint --fix

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -10,7 +10,12 @@ import {
 export const Footer = () => {
   return (
     <StackChildren flexDirection="row">
-      <IconWithLabel icon={LocationOn} label={'マップ'} fullWidth color='error' />
+      <IconWithLabel
+        icon={LocationOn}
+        label={'マップ'}
+        fullWidth
+        color="error"
+      />
       <IconWithLabel icon={ManageSearch} label={'探す'} fullWidth />
       <IconWithLabel icon={CropFree} label={'QRリーダー'} fullWidth />
       <IconWithLabel icon={Approval} label={'スタンプ'} fullWidth />
