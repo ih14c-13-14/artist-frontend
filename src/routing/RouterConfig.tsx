fa01c41aae6d094de7b5b2d995861f1eebf95b0d
aogumobc <aogumobc@gmail.com>
2023-06-27 14:55:27 +0900

    Layoutの適用

--- src/routing/RouterConfig.tsx
+++ src/routing/RouterConfig.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { Layout } from '@/components/Layout/Layout/Layout.component';
 import { Index } from '@/pages/Index';
 import { Login } from '@/pages/Login';
 import { MapShow } from '@/pages/MapShow';
@@ -22,26 +23,34 @@ export const RouterConfig = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={routes.index.path} element={<Index />} />
-        <Route path={routes.others.path} element={<NotFound />} />
-        <Route path={routes.mapShow.path} element={<MapShow />} />
-        <Route path={routes.serach.path} element={<Search />} />
-        <Route path={routes.qrRead.path} element={<QrRead />} />
-        <Route path={routes.login.path} element={<Login />} />
-        <Route path={routes.signup.path} element={<Signup />} />
-        <Route path={routes.signupConfirm.path} element={<SignupConfirm />} />
-        <Route path={routes.signupDone.path} element={<SignupDone />} />
-        <Route path={routes.passwordForget.path} element={<PasswordForget />} />
-        <Route path={routes.passwordReset.path} element={<PasswordReset />} />
-        <Route
-          path={routes.passwordResetDone.path}
-          element={<PasswordResetDone />}
-        />
-        <Route path={routes.passwordChange.path} element={<PasswordChange />} />
-        <Route
-          path={routes.passwordChangeDone.path}
-          element={<PasswordChangeDone />}
-        />
+        <Route path={routes.index.path} element={<Layout />}>
+          <Route path={routes.index.path} element={<Index />} />
+          <Route path={routes.others.path} element={<NotFound />} />
+          <Route path={routes.mapShow.path} element={<MapShow />} />
+          <Route path={routes.serach.path} element={<Search />} />
+          <Route path={routes.qrRead.path} element={<QrRead />} />
+          <Route path={routes.login.path} element={<Login />} />
+          <Route path={routes.signup.path} element={<Signup />} />
+          <Route path={routes.signupConfirm.path} element={<SignupConfirm />} />
+          <Route path={routes.signupDone.path} element={<SignupDone />} />
+          <Route
+            path={routes.passwordForget.path}
+            element={<PasswordForget />}
+          />
+          <Route path={routes.passwordReset.path} element={<PasswordReset />} />
+          <Route
+            path={routes.passwordResetDone.path}
+            element={<PasswordResetDone />}
+          />
+          <Route
+            path={routes.passwordChange.path}
+            element={<PasswordChange />}
+          />
+          <Route
+            path={routes.passwordChangeDone.path}
+            element={<PasswordChangeDone />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
