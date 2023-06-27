eb5f8aaa3f1d783f5a21b66daf6e849d7a948bbb
aogumobc <aogumobc@gmail.com>
2023-06-27 14:49:06 +0900

    Layoutフォルダ作成にともないmove

---
+++ src/components/Layout/Footer.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentMeta } from '@/utils/storybook';
+
+import { Footer } from './Footer.component';
+
+const FooterStory: ComponentMeta<typeof Footer> = {
+  component: Footer,
+};
+
+export default FooterStory;
+
+export const FooterDemo = () => {
+  return <Footer />;
+};
