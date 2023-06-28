f6b38cc07c579d5c351dfa16c2de4767c3021e64
na2na-p <na2na@na2na.dev>
2023-06-28 10:17:33 +0900

    API urlを環境ごとに動的に切り替える

--- vite.config.ts
+++ vite.config.ts
@@ -2,37 +2,44 @@
 
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), splitVendorChunkPlugin()],
-	define: {
-		'import.meta.vitest': 'undefined',
-	},
-	server: {
-		host: '0.0.0.0',
-		port: 3000,
-	},
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: 'setup-vitest.ts',
-		/**
-		 * for in-source testing.
-		 * {@link https://vitest.dev/guide/in-source.html}
-		 */
-		includeSource: ['{src,integrationTest,docs}/**/*.{ts,tsx}'],
-	},
-	build: {
-		rollupOptions: {
-			external: ['@google/model-viewer'],
-		},
-	},
-	resolve: {
-		alias: {
-			'@/': `${__dirname}/src/`,
-			'@root/': `${__dirname}/`,
-		},
-	},
-	assetsInclude: ['**/*.reality', '**/*.glb'],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+    ViteEjsPlugin({
+      apiBaseUrl: process.env.API_BASE_URL ?? 'http://localhost:4010/api/',
+    }),
+  ],
+  define: {
+    'import.meta.vitest': 'undefined',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'setup-vitest.ts',
+    /**
+     * for in-source testing.
+     * {@link https://vitest.dev/guide/in-source.html}
+     */
+    includeSource: ['{src,integrationTest,docs}/**/*.{ts,tsx}'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['@google/model-viewer'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@/': `${__dirname}/src/`,
+      '@root/': `${__dirname}/`,
+    },
+  },
+  assetsInclude: ['**/*.reality', '**/*.glb'],
 });
