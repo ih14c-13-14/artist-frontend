0ae0451a1af574bf1ebd474921825a274abb065a
na2na-p <na2na@na2na.dev>
2023-06-29 01:17:59 +0900

    PathParamを扱えるように

--- src/storybook/dataFetch/DataFetch.stories.tsx
+++ src/storybook/dataFetch/DataFetch.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta } from '@storybook/react';
 import { Fragment } from 'react';
 
 import { useAppSWR } from '@/utils/axios';
@@ -37,4 +37,21 @@ const DataFetchStory: Meta<typeof DataFetchExampleStory> = {
 
 export default DataFetchStory;
 
-export const Normal: StoryObj<typeof DataFetchExampleStory> = {};
+export const Normal = () => <DataFetchExampleStory />;
+
+export const WithPathParams = () => {
+  const { data, isLoading, error } = useAppSWR({
+    url: '/api/v1/users/{user_id}/email-change',
+    method: 'post',
+    pathParams: {
+      user_id: '1',
+    },
+    data: {
+      email: 'mail@example.com',
+    },
+  });
+
+  if (isLoading) return <div>loading...</div>;
+  if (error) return <div>error!</div>;
+  return <div>{JSON.stringify(data)}</div>;
+};
