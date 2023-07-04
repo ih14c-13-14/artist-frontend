3b663962297188d28e86cf9b6fd769a141487704
aogumobc <aogumobc@gmail.com>
2023-07-04 15:20:47 +0900

    複数コンポネントを縦に並べた例を追加

--- src/components/Layout/ArtCard/ArtCard.stories.tsx
+++ src/components/Layout/ArtCard/ArtCard.stories.tsx
@@ -18,3 +18,28 @@ export const ArtCardDemo = () => {
     />
   );
 };
+
+export const MultiArtCardsDemo = () => {
+  return (
+    <>
+      <ArtCard
+        imageSrc="" // ここどうすればいいんですか？
+        title="1しましまなしま"
+        author="1ロイド・ダロ"
+        address="1〒943-0831新潟県上越市…"
+      />
+      <ArtCard
+        imageSrc="" // ここどうすればいいんですか？
+        title="2しましまなしま"
+        author="2ロイド・ダロ"
+        address="2〒943-0831新潟県上越市…"
+      />
+      <ArtCard
+        imageSrc="" // ここどうすればいいんですか？
+        title="3しましまなしま"
+        author="3ロイド・ダロ"
+        address="3〒943-0831新潟県上越市…"
+      />
+    </>
+  );
+};
