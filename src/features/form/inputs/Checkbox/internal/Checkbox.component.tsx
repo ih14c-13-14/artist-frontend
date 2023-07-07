6e148d11eb23fef00de04070f06405e311365942
na2na-p <na2na@na2na.dev>
2023-07-07 22:23:42 +0900

    CheckboxのMUIやめる

--- src/features/form/inputs/Checkbox/internal/Checkbox.component.tsx
+++ src/features/form/inputs/Checkbox/internal/Checkbox.component.tsx
@@ -1,9 +1,44 @@
-import { MuiCheckbox } from '@/features/form/inputs/library';
+import { forwardRef, useCallback } from 'react';
 
+import { ChangeEventType } from '@/features/form/inputs/commons/types';
+
+import styles from './Checkbox.module.scss';
 import { CheckboxProps } from './Checkbox.types';
 
-const Checkbox = (props: CheckboxProps) => {
-  return <MuiCheckbox {...props} />;
-};
+const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>((props, ref) => {
+  const { choiceLabel, onChange, onChangeValue, dataTestid, ...inputProps } =
+    props;
+
+  const refCallback = useCallback(
+    (node: HTMLInputElement) => {
+      props.onBlur?.({ target: node });
+    },
+    [props]
+  );
+
+  return (
+    <>
+      <label className={styles.container}>
+        <input
+          ref={node => {
+            if (!node || !ref || 'current' in ref) return;
+            ref(node);
+            // HACK: 'onBlur'はブラウザによって差異が発生するため、ここで呼ぶ
+            // 主に、webkit系ブラウザで壊れる
+            refCallback(node);
+          }}
+          type="checkbox"
+          data-testid={dataTestid}
+          onChange={(e: ChangeEventType) => {
+            onChange?.(e);
+            onChangeValue?.(e.target.value);
+          }}
+          {...inputProps}
+        />
+        <span>{choiceLabel}</span>
+      </label>
+    </>
+  );
+});
 
 export default Checkbox;
