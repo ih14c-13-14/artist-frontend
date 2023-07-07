c1817588602a748d395053ef2a9273e5a2f90b11
na2na-p <na2na@na2na.dev>
2023-07-07 22:23:12 +0900

    checkboxのimportやめる

--- src/features/form/inputs/library/index.ts
+++ src/features/form/inputs/library/index.ts
@@ -1,12 +1,10 @@
 export {
   TextField as MuiTextField,
   FormControlLabel as MuiFormControlLabel,
-  Checkbox as MuiCheckbox,
 } from '@mui/material';
 export type {
   TextFieldProps as MuiTextFieldProps,
   FormControlLabelProps as MuiFormControlLabelProps,
-  CheckboxProps as MuiCheckboxProps,
   SxProps,
   Theme,
 } from '@mui/material';
