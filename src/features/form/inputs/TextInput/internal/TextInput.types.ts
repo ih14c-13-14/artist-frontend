419b388188814e0e099c58919635d5e1ee07a0d7
na2na-p <na2na@na2na.dev>
2023-07-09 23:09:00 +0900

    type prop 解禁

--- src/features/form/inputs/TextInput/internal/TextInput.types.ts
+++ src/features/form/inputs/TextInput/internal/TextInput.types.ts
@@ -1,6 +1,6 @@
 import type { MuiTextFieldProps } from '@/features/form/inputs/library';
 import type Length from '@/types/Length';
 
-export type TextInputProps = Omit<MuiTextFieldProps, 'type' | 'label'> & {
+export type TextInputProps = Omit<MuiTextFieldProps, 'label'> & {
   width?: Length;
 };
