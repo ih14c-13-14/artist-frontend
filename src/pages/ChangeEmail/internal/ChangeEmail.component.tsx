178a7e57efecd00da5fbcc0978670e3ed50a8495
na2na-p <na2na@na2na.dev>
2023-07-11 09:46:34 +0900

    メールアドレス変更画面1実装完了

--- src/pages/ChangeEmail/internal/ChangeEmail.component.tsx
+++ src/pages/ChangeEmail/internal/ChangeEmail.component.tsx
@@ -1,5 +1,44 @@
+import { Stack } from '@mui/system';
+
+import { TextInput } from '@/features/form/inputs/TextInput';
+import { InputWrapper } from '@/features/form/inputs/commons/InputWrapper';
+import { Button } from '@/features/ui/Button';
+import { FormPageSection } from '@/features/ui/FormPageSection';
+
+import { Fields } from './ChangeEmail.constants';
+import { useChangeEmail } from './ChangeEmail.hooks';
+import { useChangeEmailForm } from './hooks/useChangeEmailForm';
+
 const ChangeEmail = () => {
-  return <div>ChangeEmail</div>;
+  const { register, handleSubmit, getValues, isValid } = useChangeEmailForm();
+  const { onSubmit } = useChangeEmail({
+    handleSubmit,
+    getValues,
+  });
+
+  console.log(isValid);
+  return (
+    <FormPageSection
+      type="h1"
+      title="メールアドレスの変更"
+      descriptionTop="登録したいメールアドレスを入力し、確認メールを送信してください。"
+    >
+      <form onSubmit={onSubmit} noValidate>
+        <Stack gap="24px">
+          <InputWrapper label="新しいメールアドレス">
+            <TextInput
+              sx={{ width: '100%' }}
+              type="email"
+              {...register(Fields.email)}
+            />
+          </InputWrapper>
+          <Button type="submit" disabled={!isValid}>
+            送信
+          </Button>
+        </Stack>
+      </form>
+    </FormPageSection>
+  );
 };
 
 export default ChangeEmail;
