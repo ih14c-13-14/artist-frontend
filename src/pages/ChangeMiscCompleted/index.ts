d01ae1a7247cf2a10493b388bac97007557eb8c8
na2na-p <na2na@na2na.dev>
2023-07-12 21:16:27 +0900

    登録情報変更完了画面追加

---
+++ src/pages/ChangeMiscCompleted/index.ts
@@ -0,0 +1,1 @@
+export { default as ChangeMiscCompleted } from './internal/ChangeMiscCompleted.component';
---
+++ src/pages/ChangeMiscCompleted/internal/ChangeMiscCompleted.component.tsx
@@ -0,0 +1,18 @@
+import { BackToTopButton } from '@/features/navigate/BackToTopButton';
+import { FormPageSection } from '@/features/ui/FormPageSection';
+import { Stack } from '@/features/ui/Stack';
+
+import styles from './ChangeMiscCompleted.module.scss';
+
+const ChangeMiscCompleted = () => {
+  return (
+    <FormPageSection type="h1" title="登録情報変更完了">
+      <Stack gap="24px" width="100%" alignItems="center">
+        <p className={styles.description}>登録情報の変更が完了しました。</p>
+        <BackToTopButton />
+      </Stack>
+    </FormPageSection>
+  );
+};
+
+export default ChangeMiscCompleted;
