936ba13e933b5efe4685354b4ff1f4414ea25553
na2na-p <na2na@na2na.dev>
2023-07-12 20:20:07 +0900

    ちゃんとカプセル化する

--- src/features/Layout/internal/Layout/internal/Layout.component.tsx
+++ src/features/Layout/internal/Layout/internal/Layout.component.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
-import { Footer } from '@/components/Layout/Footer/Footer.component';
 import { Header } from '@/components/Layout/Header/Header.component';
 import { ErrorBoundary } from '@/features/errorHandling';
 
 import styles from './Layout.module.scss';
 import { LayoutProps } from './Layout.types';
+import { Footer } from '../../Footer';
 
 const Layout = ({ hasFooter = false }: LayoutProps) => {
   return (
