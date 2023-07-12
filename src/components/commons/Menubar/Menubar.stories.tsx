b4fc20eb22f244c7e3e81922dca8b994ed013c9a
N55039 <usukinatsuki@yahoo.co.jp>
2023-07-12 09:56:47 +0900

    不要な引数の削除

--- src/components/commons/Menubar/Menubar.stories.tsx
+++ src/components/commons/Menubar/Menubar.stories.tsx
@@ -1,5 +1,3 @@
-import { Dehaze } from '@mui/icons-material';
-
 import { ComponentMeta, StoryFn } from '@/utils/storybook';
 
 import { Menubar } from './Menubar.component';
@@ -16,5 +14,5 @@ const Template: StoryFn<typeof Menubar> = args => {
 
 export const Bar = Template.bind({});
 Bar.args = {
-  icon: Dehaze,
+  // label: 'メニュー'を渡す,
 };
--- src/components/commons/Menubar/Menubar.type.ts
+++ src/components/commons/Menubar/Menubar.type.ts
@@ -1,9 +1,6 @@
-import { SvgIconTypeMap } from '@mui/material';
 import { ButtonProps as MuiButtonProps } from '@mui/material/Button';
-import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 
 export type MenubarProps = MuiButtonProps & {
   onClick: React.MouseEventHandler<HTMLButtonElement> | undefined;
-  icon: OverridableComponent<SvgIconTypeMap>;
 };
