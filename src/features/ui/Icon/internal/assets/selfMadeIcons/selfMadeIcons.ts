e6e0fbaf0e4614d5f44c8934d0cde978c517378c
na2na-p <na2na@na2na.dev>
2023-07-13 00:25:36 +0900

    BookmarkIcon実装

--- src/features/ui/Icon/internal/assets/selfMadeIcons/selfMadeIcons.ts
+++ src/features/ui/Icon/internal/assets/selfMadeIcons/selfMadeIcons.ts
@@ -1,8 +1,10 @@
+import { ReactComponent as Bookmark } from './Bookmark.svg';
 import { ReactComponent as Map } from './Map.svg';
 import { ReactComponent as Qr } from './Qr.svg';
 import { ReactComponent as Search } from './Search.svg';
 
 const selfMadeIcons = {
+  Bookmark,
   Map,
   Qr,
   Search,
