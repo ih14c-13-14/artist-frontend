aa11e9adcdceb93c6f3b279841a41f2f1403236a
na2na-p <na2na@na2na.dev>
2023-07-14 01:30:13 +0900

    不要コンポーネント削除

--- src/components/commons/Bookmark/Bookmark.type.ts
+++
@@ -1,6 +0,0 @@
-import { ButtonProps as MuiButtonProps, SvgIconTypeMap } from '@mui/material';
-import { OverridableComponent } from '@mui/material/OverridableComponent';
-
-export type BookmarkProps = MuiButtonProps & {
-  icon: OverridableComponent<SvgIconTypeMap>;
-};
--- src/components/commons/SignIcon/SignIcon.type.ts
+++
@@ -1,6 +0,0 @@
-import { ButtonProps as MuiButtonProps, SvgIconTypeMap } from '@mui/material';
-import { OverridableComponent } from '@mui/material/OverridableComponent';
-
-export type SignProps = MuiButtonProps & {
-  icon: OverridableComponent<SvgIconTypeMap>;
-};
