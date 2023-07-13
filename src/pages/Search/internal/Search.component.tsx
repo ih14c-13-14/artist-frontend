c27916f86bc9a52c9728351192d17e049684ee9e
na2na-p <na2na@na2na.dev>
2023-07-14 02:15:53 +0900

    画面から利用できるように

--- src/pages/Search/internal/Search.component.tsx
+++ src/pages/Search/internal/Search.component.tsx
@@ -3,12 +3,13 @@ import { useState } from 'react';
 import { TabContainer } from '@/features/ui/TabContainer';
 
 import { Arts } from './Arts';
+import { Authors } from './Authors';
 
 const Search = () => {
   const [activeTab, setActiveTab] = useState('arts');
   const tabs = [
     { name: 'アート作品', value: 'arts', component: <Arts /> },
-    { name: '作者', value: 'authors', component: <Arts /> },
+    { name: '作者', value: 'authors', component: <Authors /> },
     { name: 'お気に入り', value: 'favorite', component: <Arts /> },
   ];
 
