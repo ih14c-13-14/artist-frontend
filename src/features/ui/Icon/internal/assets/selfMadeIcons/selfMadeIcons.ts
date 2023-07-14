6200c43366aa8ed26f941eecb7e56c7035d12ec9
na2na-p <na2na@na2na.dev>
2023-07-14 20:33:15 +0900

    サインアウト用Icon追加

--- src/features/ui/Icon/internal/assets/selfMadeIcons/selfMadeIcons.ts
+++ src/features/ui/Icon/internal/assets/selfMadeIcons/selfMadeIcons.ts
@@ -2,12 +2,14 @@ import { ReactComponent as Bookmark } from './Bookmark.svg';
 import { ReactComponent as Map } from './Map.svg';
 import { ReactComponent as Qr } from './Qr.svg';
 import { ReactComponent as Search } from './Search.svg';
+import { ReactComponent as SignOut } from './SignOut.svg';
 
 const selfMadeIcons = {
   Bookmark,
   Map,
   Qr,
   Search,
+  SignOut,
 };
 
 export default selfMadeIcons;
