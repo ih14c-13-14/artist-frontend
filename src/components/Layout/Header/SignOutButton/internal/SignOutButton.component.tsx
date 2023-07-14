cb54b663ae0dd94a4939caf8c19e0d8794a95b0b
na2na-p <na2na@na2na.dev>
2023-07-14 20:38:17 +0900

    SignOutButton仮配置

---
+++ src/components/Layout/Header/SignOutButton/internal/SignOutButton.component.tsx
@@ -0,0 +1,8 @@
+import { COLOR } from '@/constants/COLOR';
+import { Icon } from '@/features/ui/Icon';
+
+const SignOutButton = () => {
+  return <Icon type="SignOut" fill={COLOR.PRIMARY} />;
+};
+
+export default SignOutButton;
