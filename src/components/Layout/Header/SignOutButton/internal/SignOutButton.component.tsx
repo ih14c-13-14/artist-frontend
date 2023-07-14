7b9ed83c20042cde28a114a25516313f7433c57a
na2na-p <na2na@na2na.dev>
2023-07-14 20:48:04 +0900

    ちゃんとサインアウトさせるように

--- src/components/Layout/Header/SignOutButton/internal/SignOutButton.component.tsx
+++ src/components/Layout/Header/SignOutButton/internal/SignOutButton.component.tsx
@@ -1,12 +1,15 @@
 import { COLOR } from '@/constants/COLOR';
+import { useSignOut } from '@/features/auth';
 import { Icon } from '@/features/ui/Icon';
 import { Stack } from '@/features/ui/Stack';
 
 import styles from './SignOutButton.module.scss';
 
 const SignOutButton = () => {
+  const { signOut } = useSignOut();
+
   return (
-    <Stack onClick={() => {}} alignItems="center" gap={0}>
+    <Stack onClick={signOut} alignItems="center" gap={0}>
       <Icon type="SignOut" fill={COLOR.PRIMARY} size="24px" />
       <p className={styles.label}>サインアウト</p>
     </Stack>
